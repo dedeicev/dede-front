5883f2817061f19f13d5d11d523838e66af3b1e5
Ryan Melo <61393074+RyanMelo@users.noreply.github.com>
2023-04-25 11:33:25 -0300

    initial commit, create next project and tailwind configuration

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hola eu sou DEDE</h1>
+      
+    </>
+  )
+}
