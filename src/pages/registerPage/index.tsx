5da09d8b896e7c262dfcc981bf5f8615a7ad4e7a
Lucas Khristian <lucaasdias@gmail.com>
2023-04-27 01:19:40 -0300

    Criação da Tela de Cadastro

--- src/pages/registerPage/index.tsx
+++ src/pages/registerPage/index.tsx
@@ -27,25 +27,33 @@ export default function LoginPage() {
             <img className="w-64" src="logo-dede.svg" alt="logo-dede" />
 
             <div className="flex flex-col mt-10 w-full">
-              <h2 className="text-3xl font-bold mb-5">Login</h2>
+              <h2 className="text-3xl font-bold mb-5">Crie Sua Conta</h2>
 
-              <label>Email</label>
+              <label>Nome</label>
+              <input className="bg-[#F9F8F6] p-3 border-2 rounded" type="text" name="name" id="name" />
+
+              <label className="mt-5">Email</label>
               <input className="bg-[#F9F8F6] p-3 border-2 rounded" type="text" name="email" id="email" />
 
               <label className="mt-5">Senha</label>
               <input className="bg-[#F9F8F6] p-3 border-2 rounded" type="password" name="password" id="password" />
+     
+              <div className="flex flex-row p-3">
+                <input className="" type="checkbox" name="acceptTerms" id="acceptTerms"/>
+                <label className="ml-4 ">Concordo com os Termos e Condições</label>
+              </div>
 
               <div className="flex flex-col items-center">
                 <Link className="mt-10" href="/">
-                  <button className="bg-[#CE0203] px-10 py-4 rounded text-white">Fazer Login</button>
+                  <button className="bg-[#CE0203] px-10 py-4 rounded text-white">Cadastrar Conta</button>
                 </Link>
 
-                <Link className="mt-10" href="/registerPage">
+                <Link className="mt-10" href="/">
                   <button className="text-[#CE0203] flex flex-row">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 mr-2 rotate-180">
-                      <path fillRule="evenodd" d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm5.03 4.72a.75.75 0 010 1.06l-1.72 1.72h10.94a.75.75 0 010 1.5H10.81l1.72 1.72a.75.75 0 11-1.06 1.06l-3-3a.75.75 0 010-1.06l3-3a.75.75 0 011.06 0z" clipRule="evenodd" />
-                    </svg>
-                    Criar Conta
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-9 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+                  </svg>
+                    Voltar para o login
                   </button>
                 </Link>
               </div>
