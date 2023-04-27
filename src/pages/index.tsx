f0ba14314453cd196774cfa75753c1d70a3992ef
Ismael Rodrigues de Sousa <58527984+Hauls-Hasson@users.noreply.github.com>
2023-04-26 21:38:39 -0300

    Primeira Tentativa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Navbar from './Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
+    <Navbar />
       <h1>Hola eu sou DEDE</h1>
     </>
   )
