c4b3dfd60fbf13c202cd45caaa760167529b9181
Ryan Melo <61393074+RyanMelo@users.noreply.github.com>
2023-05-25 11:18:18 -0300

    criacao da tela de login empresarial

--- src/pages/cadastro-empresa/index.tsx
+++ src/pages/cadastro-empresa/index.tsx
@@ -58,11 +58,11 @@ export default function CadastroEmpresa() {
                         </div>
 
                         <div className="flex flex-col items-center">
-                            <Link className="mt-3" href="/">
+                            <Link className="mt-3" href="/login-empresa">
                                 <button className="bg-[#CE0203] px-10 py-4 rounded text-white">Cadastrar Conta</button>
                             </Link>
 
-                            <Link className="mt-3" href="/">
+                            <Link className="mt-3" href="/login-empresa">
                                 <button className="text-[#CE0203] flex flex-row">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-9 h-6">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
