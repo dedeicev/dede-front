19fd09119c5f5caf1a538aa2424026774ac04c18
Ryan Melo <61393074+RyanMelo@users.noreply.github.com>
2023-05-31 19:44:27 -0300

    tirando importaação dos componentes de agendamento do index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,5 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Navbar from '../components/NavBar'
-import AgendamentoAdmin from '@/components/AgendamentAdmin'
-import Agendamento from '@/components/Agendamento'
-import AgendamentoAdminAgora from '@/components/AgendamentoAdmimAgora'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,12 +7,6 @@ export default function Home() {
   return (
     <>
       <Navbar/>
-
-      <AgendamentoAdminAgora/>
-
-      <AgendamentoAdmin/>
-
-      <Agendamento/>
     </>
   )
 }
