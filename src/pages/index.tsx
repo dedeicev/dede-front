3d451551559f01f119922b318a3e32c9afb0af7e
Ryan Melo <61393074+RyanMelo@users.noreply.github.com>
2023-06-02 09:50:36 -0300

    criacao nav bar cliente

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,15 @@ import Navbar from '../components/NavBar'
 import Agendamento from '@/components/Agendamento'
 import AgendamentoAdmin from '@/components/AgendamentAdmin'
 import AgendamentoAdminAgora from '@/components/AgendamentoAdmimAgora'
+import NavBarEmpresa from '@/components/NavBarEmpresa'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <Navbar/>
+      {/* <Navbar/> */}
+      <NavBarEmpresa/>
 
       <Agendamento/>
 
